1c08067c1dedf2f41d7d9c17f1c578594ff45de7
Valentin Kopotchel <valentin.kopothcel@cybergizer.com>
2020-09-30 22:43:42 +0300

    Populate Textfiles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200930152524) do
+ActiveRecord::Schema.define(version: 20200930194240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,10 @@ ActiveRecord::Schema.define(version: 20200930152524) do
   create_table "notepad_files", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.text "text"
+    t.string "name"
+    t.index ["user_id"], name: "index_notepad_files_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,4 +46,5 @@ ActiveRecord::Schema.define(version: 20200930152524) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "notepad_files", "users"
 end
